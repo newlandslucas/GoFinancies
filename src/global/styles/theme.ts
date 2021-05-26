240f5bd2534ccf0d3195cac587f3ecde703dc480
Lucas Newlands <58925749+newlandslucas@users.noreply.github.com>
2021-05-25 23:42:53 -0300

    fix: external fonts

---
+++ src/global/styles/theme.ts
@@ -0,0 +1,24 @@
+export default {
+    colors: {
+        primary: '#151529',
+
+        secondary:  '#F7FAFC',
+        
+        sucess:  '#12A454',
+        sucess_light: 'rgba(18, 164, 84, 0.5)',
+
+        attention: '#E83F5B',
+        attention_light: 'rgba(232, 63, 91, 0.5)',
+
+        shape: '#FFFFFF',
+        title: '#3F3F5F',
+        text: '#969CB2',
+        background: '#F0F2F5',
+    },
+
+    fonts: {
+        regular: 'Poppins_400Regular',
+        medium: 'Poppins_500Medium',
+        bold: 'Poppins_700Bold'
+    }
+}
