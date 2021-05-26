afda087dc62f62451293bcff0bd2e695a5a6c17c
Lucas Newlands <58925749+newlandslucas@users.noreply.github.com>
2021-05-26 00:33:58 -0300

    fix: Header implemented

--- src/screens/Dashboard/index.tsx
+++ src/screens/Dashboard/index.tsx
@@ -1,12 +1,39 @@
 import React from 'react';
-import {  Text } from 'react-native';
 
-import { Container, Title } from './styles';
+import {
+     Container, 
+     Header,
+     UserWrapper,
+     UserInfo,
+     Photo,
+     User,
+     UserGreeting,
+     UserName,
+     Icon
+    } from './styles';
+
 
 export function Dashboard() {
     return(
         <Container>
-            <Title>GoFinancies</Title>
+            <Header>
+                <UserWrapper>
+                    <UserInfo>
+                        <Photo
+                            source={{ uri: 'https://avatars.githubusercontent.com/u/58925749?v=4'}}
+                        />
+                        <User>
+                            <UserGreeting>Olá, </UserGreeting>
+                            <UserName>Lucas</UserName>
+                        </User>
+                    </UserInfo>
+                    <Icon name="power"/>
+                  
+
+                </UserWrapper>
+
+            </Header>
+
         </Container>
     )
 }
